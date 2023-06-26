ec5aa00a544a16055a361f1fdeb52379cb2aa217
ngocdung84 <137187232+ngocdung84@users.noreply.github.com>
2023-06-26 15:37:44 +0700

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
